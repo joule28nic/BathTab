5b1ccd3fbda35faddbc9f029d2f6b30f926609fe
joule28nic <153733673+joule28nic@users.noreply.github.com>
2024-05-02 11:23:51 +0000

    enum

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,7 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  enum sex: { not_selected: 0, male: 1, female: 2}, _prefix: true
+  enum age: { not_selected: 0, teens: 1, twenties: 2, thirties: 3, forties: 4, fifties: 5, sixties: 6}, _prefix: true
 end
