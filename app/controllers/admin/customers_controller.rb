952ad513ce6c3c3755de6fa5f50c3731308d275e
joule28nic <153733673+joule28nic@users.noreply.github.com>
2024-06-03 12:43:41 +0000

    管理者_会員情報詳細画面_レイアウト

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -22,7 +22,7 @@ class Admin::CustomersController < ApplicationController
   private
 
   def customer_params
-    params.repuire(:customer).permit(:is_active)
+    params.require(:customer).permit(:is_active)
   end
 
 end
