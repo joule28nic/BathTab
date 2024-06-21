17feb105136200c847fa10bbdf43be3fc4630afa
joule28nic <153733673+joule28nic@users.noreply.github.com>
2024-06-21 11:46:43 +0000

    エリア検索

--- app/controllers/public/facilities_controller.rb
+++ app/controllers/public/facilities_controller.rb
@@ -4,6 +4,7 @@ class Public::FacilitiesController < ApplicationController
   def index
     @prefectures = Prefecture.all
     @equipment = Equipment.all
+
     if @q
       @facilities = @q.result.page(params[:page]).per(10)
     else
@@ -19,7 +20,28 @@ class Public::FacilitiesController < ApplicationController
   private
 
   def set_q
-    @q = Facility.ransack(params[:q])
-  end
+    # if params[:q] && params[:q][:equipment_id_eq]
+    #   params[:q][:equipment_id_eq] = params[:q][:equipment_id_eq].reject(&:blank?).map(&:to_i)
 
+    #   @a = EquipmentRelationship.where(equipment_id: params[:q][:equipment_id_eq])
+
+    #   eq_ids = @a.pluck(:facility_id)
+
+
+    #   @facilities = Facility.where(id: eq_ids)
+      # @facilities = Facility.joins(:equipment_relationships)
+      #                       .where(equipment_relationships:{equipment_id: params[:q][:equipment_id_eq] })
+      #                       .group('facility_id')
+      #                       .having('COUNT(equipmentRelationships.equipment_id) = ?',params[:q][:equipmnt_id_eq]&.size || 0)
+      # @q = Facility.ransack(id: eq_ids)
+
+      #facilitiesからfacility_idを抽出L28
+      #Facility.ransack(id: 上で取得したid)
+
+    #   @q =  @facilities.ransack()
+    # else
+
+      @q = Facility.ransack(params[:q])
+    # end
+  end
 end
